2e171cabab600df0d07810f4fd05408023dc8682
JaelAguilar <71282327+JaelAguilar@users.noreply.github.com>
2022-03-30 13:55:31 -0600

    Removed index text and added counter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <img src="../assets/logo.png" alt="Vue Logo">
+    <div class="counter">
+        {{counter}}
+    </div>
+    <div class="buttons">
+        <button @click="bajarContador()">-</button>
+        <button @click="subirContador()">+</button>
+    </div>
 </template>
 
 <script>
@@ -11,8 +15,29 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data(){
+      return {counter:0}
+  },
+  methods: {
+      bajarContador(){
+          this.counter--
+      },
+      subirContador(){
+          this.counter++
+      }
   }
 }
 </script>
+<style>
+    div{
+        margin-bottom:10px;
+    }
+    .counter{
+        font-size:80px;
+    }
+    .buttons button{
+        font-size:40px;
+        width:100px;
+        margin:0 10px;
+    }
+</style>
